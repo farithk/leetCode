a143120b62bb8680b7f79b513b116b84b68b8af2
unknown <farithmauriciocomas@gmail.com>
2021-04-10 15:08:00 -0500

    first commit

---
+++ 1TwoSum.js
@@ -0,0 +1,21 @@
+var twoSum = function(nums, target) {
+    let positions = [];
+    
+    if(nums.length >= 2 && nums.length <= 1000 && target >= Math.pow(-10, 9) && target <= Math.pow(10, 9)){
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i] >= Math.pow(-10, 9) && nums[i] <= Math.pow(10, 9)){
+                for(let j = i + 1; j < nums.length; j++){
+                    //console.log(nums[i] + nums[j]);
+                    if((nums[i] + nums[j]) === target){
+                        positions = [i, j];
+                    }
+                }
+            }
+        }
+    }
+   
+    
+    return positions;
+};
+
+console.log(twoSum([2,7,11,15], 9));
